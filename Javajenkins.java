55cc830285c60d89db3458c84688d06062faa81b
smooky <karim.kameli71@gmail.com>
2023-07-20 14:46:30 +0200

    modif ok

--- Javajenkins.java
+++ Javajenkins.java
@@ -1,7 +1,7 @@
 public class Javajenkins {
     public static void main(String[] args){
         System.out.println("Bonjour, World Devops et test web hook!");
-        Additionnner(1, 2);
+        // Additionnner(1, 2);
     }
 
     // public static int Additionnner(int a, int b){
